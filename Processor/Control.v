8184849d83c4a0f8306a859eb3879d8637d07f3f
tarmak4317 <167742622+tarmak4317@users.noreply.github.com>
2025-04-25 00:29:02 +0530

    Add files via upload

--- Processor/Control.v
+++ Processor/Control.v
@@ -1,6 +1,7 @@
 module Control(input clk, input [3:0]portin, input [3:0]instr, output reg [3:0]portout);
 	reg mem_enable;
 	reg alu_enable;
+	reg transfer_enable;
 	reg rw;
 	wire [3:0]mem_out;
 	wire [3:0]alu_out;
@@ -13,8 +14,11 @@ module Control(input clk, input [3:0]portin, input [3:0]instr, output reg [3:0]p
 			Y <= mem_out;
 		if(alu_enable)
 			Y <= alu_out;
+		if(transfer_enable)
+			Y1 <= Y;
 		alu_enable <= 1'b0;
 		mem_enable <= 1'b0;
+		transfer_enable <= 1'b0;
 		case(instr)
 			4'b0000: alu_enable <= 1'b1;
 			4'b0001: alu_enable <= 1'b1;
@@ -30,7 +34,7 @@ module Control(input clk, input [3:0]portin, input [3:0]instr, output reg [3:0]p
 				 end
 			4'b0110: Y <= portin;
 			4'b0111: portout <= Y;
-			4'b1000: Y1 <= Y;
+			4'b1000: transfer_enable <= 1'b1;
 			4'b1001: begin
 				 	mem_enable <= 1'b0;
 				 	alu_enable <= 1'b0;
--- Processor/Processor_Test.v
+++ Processor/Processor_Test.v
@@ -4,18 +4,23 @@ module Processor_Test;
 	wire [3:0] portout;
 	reg clk;
 	initial begin
-		clk = 1'b0;
+		clk = 1'b1;
 		forever #5 clk = ~clk;
 	end
 	Control uut(.clk(clk), .portin(portin), .instr(instr), .portout(portout));
 	initial begin
 		$dumpfile("Processor.vcd");
 		$dumpvars(0,uut);
-		instr = 4'b1001;#10;
+		portin = 4'b0000;instr = 4'b1001;#10;
 		portin = 4'b0011;instr = 4'b0110;#10;
 		portin = 4'b0000;instr = 4'b0100;#10;
-		instr = 4'b0101;#10;
-		instr = 4'b0111;#10;
+		portin = 4'b0011;instr = 4'b0110;#10;
+		portin = 4'b0001;instr = 4'b0100;#10;
+		portin = 4'b0000;instr = 4'b0101;#10;
+		portin = 4'b0000;instr = 4'b1000;#10;
+		portin = 4'b0001;instr = 4'b0101;#10;
+		portin = 4'b0000;instr = 4'b0000;#10;
+		portin = 4'b0000;instr = 4'b0111;#10;
 		#20;
 		$stop;
 	end
